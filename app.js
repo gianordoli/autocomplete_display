5be7c4de187e16be89f54ce0c9162591e31b662a
gianordoli <gianordoligabriel@yahoo.com.br>
2015-04-19 23:47:14 -0400

    Parsing and Filtering data

---
+++ app.js
@@ -0,0 +1,142 @@
+/*-------------------- MODULES --------------------*/
+var		express = require('express'),			  // Run server
+	 bodyParser = require('body-parser'),		  // Parse requests
+	MongoClient = require('mongodb').MongoClient, // Access database
+			  _ = require('underscore');		  // Filtering/sorting
+
+/*-------------------- SETUP --------------------*/
+var app = express();
+// .use is a middleware
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(bodyParser.json());
+app.use(function(req, res, next) {
+    // Setup a Cross Origin Resource sharing
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+    console.log('incoming request from ---> ' + ip);
+    // Show the target URL that the user just hit
+    var url = req.originalUrl;
+    console.log('### requesting ---> ' + url);
+    next();
+});
+
+app.use('/', express.static(__dirname + '/public'));
+/*-----------------------------------------------*/
+
+
+var imagesInDB = [];
+var youtubeInDB = [];
+
+function init(){
+    MongoClient.connect('mongodb://127.0.0.1:27017/thesis', function(err, db) {
+        loadImages(db, function(data){
+            
+            imagesInDB = data;
+
+            loadYoutube(db, function(data){
+                
+                youtubeInDB = data;
+
+            });
+
+        });
+    });    
+}
+
+function loadImages(db, callback){
+
+    console.log('Called loadImages.');
+
+    var imagesCollection = db.collection('images');
+
+    imagesCollection.find({}).toArray(function(err, results) {            
+        console.log('Found ' + results.length + ' images.');
+        callback(results);
+    });
+}
+
+function loadYoutube(db, callback){
+    
+    console.log('Called loadYoutube.');
+
+    var youtubeCollection = db.collection('youtube');
+
+    youtubeCollection.find({}).toArray(function(err, results) {            
+        console.log('Found ' + results.length + ' videos.');
+        callback(results);
+        db.close(); // Let's close the db 
+    });
+}
+
+app.post('/start', function(request, response) {
+
+    console.log(request.body['date']);
+    var date1 = new Date(parseInt(request.body['date']) - 86400000);
+    var date2 = new Date(parseInt(request.body['date']));
+
+    console.log(date1.getFullYear() + '/' + date1.getMonth() + '/' + date1.getDate() + ' - ' + date1.getHours() + ':' + date1.getMinutes() + ':' + date1.getSeconds());
+    console.log(date2.getFullYear() + '/' + date2.getMonth() + '/' + date2.getDate() + ' - ' + date2.getHours() + ':' + date2.getMinutes() + ':' + date2.getSeconds());
+
+    MongoClient.connect('mongodb://127.0.0.1:27017/thesis', function(err, db) {
+        
+        console.log('Connecting to DB...');
+        
+        if(err) throw err;
+        
+        console.log('Connected.');
+
+        var recordsCollection = db.collection('records');
+
+        recordsCollection.find({
+            'date': { '$gt': date1, '$lte': date2 },
+            'language_code': 'pt-BR'
+        }).toArray(function(err, results) {
+            // console.dir(results);
+            // console.log(results.length);
+
+            // Getting youtube and images url from the other DBs
+            for(var i = 0; i < results.length; i++){
+                if(results[i]['service'] == 'images'){
+                    // console.log(results[i]['query']);
+                    var record = _.find(imagesInDB, function(item, index, list){
+                        // console.log(item['query']);
+                        return item['query'] == results[i]['query'];
+                    });
+                    // console.log(results[i]['query']);
+                    // console.log(record);
+                    results[i]['url'] = record['url'];
+                    // console.log(results[i]);
+
+                }else if(results[i]['service'] == 'youtube'){
+                    // console.log(results[i]['query']);
+                    var record = _.find(youtubeInDB, function(item, index, list){
+                        // console.log(item);
+                        // console.log(item['query']);
+                        return item['query'] == results[i]['query'];
+                    });
+                    // console.log(results[i]['query']);
+                    // console.log(record);
+                    results[i]['videoId'] = record['videoId'];
+                    results[i]['thumbnail'] = record['thumbnail'];
+                    // console.log(results[i]);
+                }
+            }
+            response.json(results);
+
+            db.close(); // Let's close the db 
+        });         
+    });
+});
+
+/*----------------- INIT SERVER -----------------*/
+var PORT = 3000; //the port you want to use
+app.listen(PORT, function() {
+    console.log('Server running at port ' + PORT + '. Ctrl+C to terminate.');
+});
+
+init();
--- public/js/main.js
+++ public/js/main.js
@@ -3,6 +3,43 @@
 var app = {};
 
 app.init = function() {
+
+	var convertToServerTimeZone = function(date){
+	    //EST
+	    var localToUtcOffsetMin = new Date().getTimezoneOffset();
+	    var localToUtcOffsetMillis = localToUtcOffsetMin * 60000;
+	    var clientDateMillis = Date.parse(new Date(date));
+	    var serverDateMillis = clientDateMillis + localToUtcOffsetMillis;
+	    return serverDateMillis;
+	    // offset = -5.0
+	    // clientDate = new Date(date);
+	    // utc = clientDate.getTime() + (clientDate.getTimezoneOffset() * 60000);
+	    // serverDate = new Date(utc + (3600000*offset));
+	    // console.log(serverDate.toLocaleString());
+	};
+
+	var loadData = function(){
+
+		var startDate = new Date(2015, 02, 24);
+		console.log(startDate.getTime());
+		// console.log(startDate.getHours());
+		// startDate = convertToServerTimeZone(startDate);
+		// console.log(startDate.getHours());
+
+		$.post('/start', {
+			date: startDate.getTime()
+		}, function(response) {
+	        // console.log(response);
+	        if(response.error){
+	        	throw response.error	
+	        }else{
+	        	console.log(response);
+	        }
+	    });				
+	}
+
+	loadData();
+
 	var obj = {
 	    videoId: 'jtDnmVjPfvM',
 	    thumbnail: 'https://i.ytimg.com/vi/jtDnmVjPfvM/hqdefault.jpg'
